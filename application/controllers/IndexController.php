e8b3d546a18851bce2c69174ae3b56c4b37eb6a1
André Pinheiro <apinheiro@lnxd3i02.(none)>
2010-08-09 21:23:13 -0300

    Alterando a estrutura básica do site

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,16 +3,50 @@
 class IndexController extends Zend_Controller_Action
 {
 
+    protected $noticiaMapper;
+
     public function init()
     {
         /* Initialize action controller here */
+        $this->noticiaMapper = new Application_Model_NoticiasMapper();
+        $this->view->noticias = $this->noticiaMapper->getUltimasNoticias();
+
     }
 
     public function indexAction()
     {
         // action body
+        $where = array('in_home = ?'=>'1',"(dt_limite > ? or dt_limite = '0000-00-00') "=>date('Y-m-d H:i:s'));
+        $this->view->capaNoticias = $this->noticiaMapper->fetchAll($where);
+    }
+
+    public function sobreRevistaAction()
+    {
+        // action body
+//        $noticiaMapper = new Application_Model_NoticiasMapper();
+//        $this->view->noticias = $noticiaMapper->getUltimasNoticias();
+    }
+
+    public function apoioAction()
+    {
+        // action body
+//        $noticiaMapper = new Application_Model_NoticiasMapper();
+//        $this->view->noticias = $noticiaMapper->getUltimasNoticias();
+    }
+
+    public function nossaEquipeAction()
+    {
+        // action body
+ //       $noticiaMapper = new Application_Model_NoticiasMapper();
+ //       $this->view->noticias = $noticiaMapper->getUltimasNoticias();
     }
 
 
 }
+
+
+
+
+
+
 
