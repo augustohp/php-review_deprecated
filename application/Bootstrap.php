804c7a340172a42ccb817ba3d31b5c2a45547759
André Pinheiro <apinheiro@lnxd3i02.(none)>
2010-08-11 21:20:54 -0300

    Removendo arquivos inexistentes

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -4,5 +4,17 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
 
+    public function init(){
+
+        $twitter = new Zend_Service_Twitter('phpreview','ciband2010');
+        $resposta = $twitter->accountVerifyCredentials();
+
+        $linhas = $twitter->statusUserTimeline();
+
+        echo var_dump($linhas);
+
+        // finalizando conexão com o Twitter.
+        $twitter->accountEndSession();
+    }
 }
 
